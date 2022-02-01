88041b60643f34b8cfe72bc5e28b08bf82a35630
yuta-ike <38308823+yuta-ike@users.noreply.github.com>
2022-02-01 19:27:05 +0900

    setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next"
+
+const Home: NextPage = () => {
+  return (
+    <div className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+      <p>Hello, world</p>
+    </div>
+  )
+}
+
+export default Home
